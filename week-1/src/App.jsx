a5f6a1a8579b0b5af34485e0eed6bda29835d053
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-25 17:38:28 +0900

    state(1)

--- week-1/src/App.jsx
+++ week-1/src/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import Child from "Child";
+import { useState } from "react";
 
 function App() {
-    const name = "test";
+    // state를 사용하는 이유 : UI를 바꾸기 위해서다!
+    // hook
+    // const [state, setState] = useState('절대값');
+    // const [count, setCount] = useState(0);
+    // const [todoList, setTodoList] = useState[[]];
+
+    // const [name, setName] = useState('김할아버지')
+    const [fruits, setFruits]= useState("")
+
     return (
-        <Child age={21} name={name}>
-            이름
-        </Child>
-    );
+        <div> 과일 : <input 
+                        value={fruits} 
+                        onChange={(event)=>{setFruits(event.target.value)}}
+                        // 이벤트의 경우 target이 필요. (콘솔로그 + F12 참고)
+                        />
+        <br/>
+        {fruits}
+    </div>) 
 }
 
 export default App;
