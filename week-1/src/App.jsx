c67bbde5dbe9d2139be9373680c57574feff36b9
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-25 23:18:52 +0900

    +, - 를 이용한 카운터 앱 만들기

--- week-1/src/App.jsx
+++ week-1/src/App.jsx
@@ -1,23 +1,14 @@
 import React, { useState } from "react";
 
 function App() {
-    const [obj, setObj] = useState({
-        name : 'sy',
-        age : 10
-    });
-
-    console.log(obj)
+    const [num, setNum] = useState(0);
+    // [] 안에 있는 num, setNum은 fn이다. 배열처럼 보이지만... 아님
 
     return (
-    <div>
-        <div>{obj.name}</div>
-        <button
-        onClick={()=>{
-            const obj2 = {...obj}; // obj를 복사를 하는데 완전히 새로운 방법으로 복사하는 방법. 주소값이 새로 할당됨.
-            // 배열이나 객체 나오면 스프레드 문법 쓸 준비 하세요.
-            obj2.name = 'shasha';
-            setObj(obj2);
-            }}>버튼</button>
+    <div style={{margin:'50px'}}>
+        <p>{num}</p>
+        <button onClick={()=>{setNum(num-1)}}>-</button>
+        <button onClick={()=>{setNum(num+1)}}>+</button>
     </div>) 
 }
 
