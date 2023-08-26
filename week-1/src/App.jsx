e5da39e3e7b67b6504b96cf4cea36f5a6fa5dbee
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-26 23:35:02 +0900

    추가, 삭제

--- week-1/src/App.jsx
+++ week-1/src/App.jsx
@@ -2,23 +2,54 @@ import React, { useState } from "react";
 import "./App.css"; // css 연결
 
 function App() {
-    // 분리 안한 css
-    const style = {
-        color : 'green'
+    const [users, setUsers] = useState([
+        {id: 1, age: 20, name: '김뫄뫄'},
+        {id: 2, age: 19, name: '박뫄뫄'},
+        {id: 3, age: 27, name: '최뫄뫄'},
+        {id: 4, age: 12, name: '장뫄뫄'},
+        {id: 5, age: 30, name: '정뫄뫄'},
+    ]);
+
+    const [name, setName]= useState('');
+    const [age, setAge] = useState('');
+
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
     }
 
-    const testArr = ['감자','고구마','오이','가지','옥수수']
+    const ageChangeHandler = (event) => {
+        setAge(event.target.value);
+    }
+
+    const clickAddBtn = () =>{
+        const newUser = {
+            id : users.length + 1,
+            age : age,
+            name : name
+        }
+
+        setUsers([...users, newUser]);
+    }
+
+    const clickRemoveBtn = (id) => {
+        const newUsers = users.filter((user)=> user.id !== id);
+        setUsers(newUsers)
+    }
 
     return (
-    <div>
-        {testArr
-        .filter((item)=>{
-            return item !== '오이';
-        }).map((item)=>{
-            return <div className="vege">{item}</div>
-        }) 
-        }
-    </div>) 
+        <>
+        <div>
+            이름: <input value={name} onChange={nameChangeHandler} type="text" />
+            나이: <input value={age} onChange={ageChangeHandler} type="number" />
+            <button onClick={clickAddBtn}>추가</button>
+        </div>
+        {users.map((item) => (
+            <div key={item.id}>{item.age} - {item.name}
+            <button onClick={()=>{clickRemoveBtn(item.id)}}>X</button>
+            </div>
+        ))}
+        </>
+    )
 }
 
 export default App;
