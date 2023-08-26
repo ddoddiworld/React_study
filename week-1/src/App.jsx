5f17690fdc916f47bc1599df780355ca28c876af
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-26 20:47:07 +0900

    filter, map

--- week-1/src/App.jsx
+++ week-1/src/App.jsx
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 import "./App.css"; // css 연결
 
 function App() {
-    // 분리 안한 css 작성 법
+    // 분리 안한 css
     const style = {
         color : 'green'
     }
 
+    const testArr = ['감자','고구마','오이','가지','옥수수']
+
     return (
     <div>
-        <ul className="vege" style={style}>
-            <li>감자</li>
-            <li>고구마</li>
-            <li>오이</li>
-            <li>가지</li>
-            <li>옥수수</li>
-        </ul>
+        {testArr
+        .filter((item)=>{
+            return item !== '오이';
+        }).map((item)=>{
+            return <div className="vege">{item}</div>
+        }) 
+        }
     </div>) 
 }
 
