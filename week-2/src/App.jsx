6215c5fc9539766111ed1c83d4e9b8a1e238a64a
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-28 15:58:58 +0900

    id 10자리면 pw로 넘기게 만들기

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import { useState } from 'react';
-import { useRef } from 'react';
+import React, {useState, useEffect, useRef } from 'react'
 
 function App() {
-    const [count, setCount] = useState(0);
-    const countRef = useRef(0);
+    const [id, setId] = useState('')
+    const idRef = useRef('');
+    const pwRef = useRef('');
 
-    const style = {border: '1px solid black', margin: '10px', padding: '10px'}
+    // 화면이 랜더링될 때 어떤 작업을 하고 싶다! : useEffect
+    useEffect(()=>{
+        idRef.current.focus();
+        // pwRef.current.focus();
+    }, [])
 
-    const plusStateCountButtonHandler = () => {
-        setCount(count+1) // 랜더링 일어남
-    }
+    // useEffect
+    useEffect(()=>{
+        if(id.length >= 10) {
+            pwRef.current.focus();
+        }
+    }, [id]) // []안에 넣으면 넣은 값이 바뀔때마다 업데이트 해 줘야 하기 때문에 넣어야 함!
 
-    const plusRefCountButtonHandler = () => {
-        countRef.current ++;
-        console.log(countRef.current) // 랜더링 안일어남 -> 콘솔창으로 확인 가능.
-    }
+    // useState
+    // const idChk = (event) => {
+    //     setId(event.target.value);
+    //     // 리액트에서 state -> 배치 업데이트... 1개씩 늦게 업데이트가 됨!
+    //     if(id.length === 9) {
+    //         pwRef.current.focus();
+    //     } else {
+    //         idRef.current.focus();
+    //     }
+    // }
 
     return (
-        <>
-            <div style={style}>State 영역입니다. {count} <br/> <button onClick={plusStateCountButtonHandler}>state 증가</button></div>
-            <div style={style}>Ref 영역입니다. {countRef.current} <br/> <button onClick={plusRefCountButtonHandler}>Ref 증가</button></div>
-        </>
+    <>
+    <div>
+        아이디 : <input type="text" value={id} onChange={(event)=>{setId(event.target.value)}} ref={idRef}></input>
+    </div>
+    <div>
+        비밀번호 : <input type="password" ref={pwRef}></input>
+    </div>
+    </>
     )
 }
 
